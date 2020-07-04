987c6c7527c0d440ca7c089be0a7cfa321986c94
Satria H R Harsono <satriahrh@gmail.com>
2020-07-04 08:45:23 +0700

    preflight

--- app/web/main.go
+++ app/web/main.go
@@ -75,6 +75,7 @@ func main() {
 	router.Use(func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			w.Header().Set("Access-Control-Allow-Origin", os.Getenv("CORS_ALLOWED_ORIGINS"))
+			w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 			next.ServeHTTP(w, r)
 		})
 	})
